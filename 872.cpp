c22ef26bc99ed8bc617c68074ad15cd101f11f21
IAmAadesh <119813921+IAmAadesh@users.noreply.github.com>
2022-12-08 15:08:32 +0530

    Create 872.cpp

---
+++ 872.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void addNode(TreeNode* root, vector<int>&v){
+        if (!root)  return;
+        if(!(root->left) && !(root->right)){
+            v.push_back(root->val);
+            return;
+        }
+        addNode(root->left,v);
+        addNode(root->right,v);
+        return;
+    }
+
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int>v1,v2;
+        addNode(root1,v1);
+        addNode(root2,v2);
+
+        if(v1.size()==v2.size()){
+            for(int i=0;i<v1.size();i++){
+                if(v1[i]!=v2[i])return false;
+            }
+            return true;
+        }
+        return false;
+    }
+};
